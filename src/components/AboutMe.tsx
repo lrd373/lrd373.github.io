bcd1c3f99b5e97bd317a24343edfb12763bd5745
Lauren <lauren@laurendelucia.com>
2026-08-20 11:15:23 -0400

    Improve short bio on Home page

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,21 +1,25 @@
 export const AboutMe = () => {
   return (
     <div className="flex overflow-hidden px-6 py-10 md:max-w-175 md:py-24">
-      <p className="font-secondary text-sm leading-8 md:text-lg">
+      <p className="font-secondary text-sm leading-8 md:text-xl">
         Soprano and New Jersey native Lauren DeLucia utilizes the unique “depth
         of her low tones and fierce declamatory power” (South Florida Classical
         Review) in a wide range of repertoire from early to contemporary music
-        and U.S. premieres. Her operatic credits include the title role in
-        Armide by Jean-Baptiste Lully at the Miami Music Festival, Casilda in
-        The Gondoliers with the Gilbert and Sullivan Light Opera Company of Long
-        Island, and First Lady in The Magic Flute with the Bach Society of
-        Columbia University. She is an experienced chorister as well, having
-        appeared as a soloist in J.S. Bach&apos;s Magnificat and Vivaldi&apos;s
-        Gloria in D Major, and as a soprano ensemble member in the U.S. premiere
-        of Carl Davis&apos;s Last Train to Tomorrow at Alice Tully Hall and in
-        Handel&apos;s Messiah with Camerata New York. In 2020, Lauren earned her
-        Master of Music in Voice from the Mannes School of Music in New York
-        City.
+        and U.S. premieres. Her operatic credits include the title role in{" "}
+        <em>Armide</em> by Jean-Baptiste Lully at the Miami Music Festival,
+        Casilda in <em>The Gondoliers</em> with the Gilbert and Sullivan Light
+        Opera Company of Long Island, and First Lady in <em>The Magic Flute</em>{" "}
+        with the Bach Society of Columbia University (BSCU). She is an
+        experienced chorister as well, having made her Carnegie Hall debut this
+        year with the Arcadia Symphony chorus for their America 250 celebration.
+        She has also appeared as a soloist with Voices Chorale NJ in Mozart's{" "}
+        <em>Requiem</em>, Vivaldi&apos;s <em>Gloria</em>, Haydn's Lord Nelson
+        Mass, and Saint-Saëns's Christmas Oratorio, and with the BSCU in J.S.
+        Bach&apos;s <em>Magnificat</em>. She was a soprano choir member in
+        Handel&apos;s Messiah with Camerata New York and in the U.S. premiere of
+        Carl Davis&apos;s <em>Last Train to Tomorrow</em> at Alice Tully Hall.
+        In 2020, Lauren earned her Master of Music in Voice from the Mannes
+        School of Music in New York City.
       </p>
     </div>
   );
