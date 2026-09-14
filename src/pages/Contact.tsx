f70fca60b83f4ccfbff2c4a3ac542fd6331d8a6a
Lauren <lauren@laurendelucia.com>
2026-09-14 14:10:28 -0400

    Larger desktop Contact hero image

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -8,9 +8,9 @@ export const Contact = () => {
       <img
         src={smileByFountain}
         alt=""
-        className="h-[calc(70vh)] w-full max-w-[1800px] object-cover object-bottom lg:inline-block lg:h-[calc(75vh)]"
+        className="h-[calc(70vh)] w-full max-w-[1800px] object-cover object-bottom lg:inline-block lg:h-[calc(90vh)]"
       />
-      <div className="flex w-full max-w-187.5 flex-col gap-4 self-center px-6 pt-16 pb-32 md:px-12 md:pt-24 md:pb-48">
+      <div className="flex w-full max-w-187.5 flex-col gap-4 self-center px-6 pt-16 pb-32 md:px-12 md:pt-24 md:pb-48 lg:pt-10">
         <PageHeader>Get In Touch</PageHeader>
         <ContactForm />
       </div>
